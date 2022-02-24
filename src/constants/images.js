af34e41326f0cb1dc86b59d5e9aa895363a24681
Andrey Gomez <afgomezvonline@gmail.com>
2022-02-23 22:31:28 -0500

    header  and about finished

--- src/constants/images.js
+++ src/constants/images.js
@@ -4,7 +4,7 @@ import api from "../assets/api.png";
 import cpp from "../assets/cpp.png";
 import css from "../assets/css.png";
 import figma from "../assets/figma.png";
-import flutter from "../assets/flutter.png";
+import github from "../assets/github.png";
 import git from "../assets/git.png";
 import graphql from "../assets/graphql.png";
 import html from "../assets/html.png";
@@ -17,6 +17,7 @@ import redux from "../assets/redux.png";
 import sass from "../assets/sass.png";
 import typescript from "../assets/typescript.png";
 import vue from "../assets/vue.png";
+import mongo from "../assets/mongo.png";
 
 import about01 from "../assets/about01.png";
 import about02 from "../assets/about02.png";
@@ -42,7 +43,7 @@ export default {
   cpp,
   css,
   figma,
-  flutter,
+  github,
   git,
   graphql,
   html,
@@ -55,6 +56,7 @@ export default {
   sass,
   typescript,
   vue,
+  mongo,
   about01,
   about02,
   about03,
